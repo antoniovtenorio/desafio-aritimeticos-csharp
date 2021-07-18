138ac1b76a1c19d487dbcbb03ea20e126808d234
Antonio Tenorio <antonio.v.tenorio@outlook.com>
2021-07-18 17:37:04 -0300

    Iniciado o projeto do desafio de matemática C# GFT

---
+++ DesafioAritmeticos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DesafioAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            NotasMoedas.mostrarResultado();
+        }
+    }
+}
